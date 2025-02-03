72c6effac3fcda5c43a238c59d2df90c6ae16f38
Atom13 <64770551+aToom13@users.noreply.github.com>
2025-02-03 22:39:19 +0300

    Update gallery.js

--- Galeri/gallery.js
+++ Galeri/gallery.js
@@ -1,20 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   const images = [
+        'foto1.jpg'
         'foto2.jpg',
         'foto3.jpg',
         'foto4.jpg',
         'foto5.jpg',
         'foto6.jpg',
-        'foto8.jpg'
+        'foto7.jpg',
+        'foto8.jpg',
+        'foto9.jpg',
+        'foto10.jpg',
+        'foto11.jpg',
+        'foto12.jpg'
     ];
 
   const photoGrid = document.getElementById('photoGrid');
   const captions = [
-        "İlk Buluşmamız 🌸",
-        "Gülen Anılarımız 😊",
-        "Mutluluk Dolu Bir Gün ☀️",
-        "Birlikte Keşiflerimiz 🗺️",
-        "Unutulmaz Anlarımız 💖"
+        "Bunların hepsine",
+        "Başlık atmam lazım",
+        "Ama",
+        "Atamam sevgilimm",
+        "Çünkü bu anılar",
+        "Bir kaç",
+        "İle anlatılamicak",
+        "Kadar muhteşem anılar",
+        "Bu anılarda",
+        "Bana eşlik ettiğin için",
+        "Teşekkürlerrr",
+        "Yenilerini eklemeye ne dersinn sevgilimm ❤️"
     ];
 
   images.forEach((img, index) => {
